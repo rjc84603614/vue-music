2370ce0c0352a53f2adf5495c3315be49f92eb55
Mp-dfu <634073052@qq.com>
2017-11-04 01:07:28 +0800

    DAY11/4

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -7,6 +7,16 @@ export function addClass(el, className) {
   el.className = newClass.join(' ')
 }
 
+export function getData(el, name, val) {
+  const prefix = 'data-'
+  name = prefix + name
+  if (val) {
+    return el.setAttribute(name, val)
+  } else {
+    return el.getAttribute(name)
+  }
+}
+
 export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
